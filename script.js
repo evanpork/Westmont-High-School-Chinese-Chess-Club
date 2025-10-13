2e4d57c746ba868cce2575dd32345de382885f69
evanpork <evan.r.du@gmail.com>
2025-10-13 00:51:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// Fade transition between pages
-document.addEventListener("DOMContentLoaded", () => {
-  const anchors = document.querySelectorAll("a");
+// Smooth fade-out transitions between pages
+document.addEventListener('DOMContentLoaded', () => {
+  const anchors = document.querySelectorAll('a');
   for (let a of anchors) {
     if (a.hostname === window.location.hostname && !a.target) {
-      a.addEventListener("click", function (e) {
+      a.addEventListener('click', e => {
         if (a.href !== window.location.href) {
           e.preventDefault();
-          document.body.classList.add("fade-out");
-          setTimeout(() => {
-            window.location = a.href;
-          }, 800);
+          document.body.classList.add('fade-out');
+          setTimeout(() => window.location = a.href, 800);
         }
       });
     }
   }
 });
 
-// Fade-out animation class
 const style = document.createElement("style");
 style.innerHTML = `
   .fade-out {
